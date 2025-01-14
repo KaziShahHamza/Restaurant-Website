37d8ba3a66dad3fcde22c080de99a43f7f58e98d
soulfulDreamer <kazishahhamza@outlook.com>
2025-01-15 00:49:23 +0600

    tailwind installed

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
 
   return (
-    <>
-    <h1>Restaurent Website 2</h1>
-    </>
+    <h1 className="text-10xl font-bold underline ">
+      Hello SMUCT!
+    </h1>
   )
 }
 
